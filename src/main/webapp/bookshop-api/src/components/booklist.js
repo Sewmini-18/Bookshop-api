6f91947017ff275f6a61e0b4982ac6b758db7897
Sewmini Dissanayaka <61307442+Sewmini-18@users.noreply.github.com>
2020-10-23 00:05:55 +0530

    get request using axios

--- src/main/webapp/bookshop-api/src/components/booklist.js
+++ src/main/webapp/bookshop-api/src/components/booklist.js
@@ -1,18 +1,36 @@
 import React from 'react';
-import { Card, Container, Table } from 'react-bootstrap'
+import { Card, Container, Table,ButtonGroup,Button, Image } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faList } from '@fortawesome/free-solid-svg-icons'
+import { faList, faTrash, faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios'
 
 class BookList extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            books:[]
+        }
+    }
+    componentDidMount(){
+        axios.get("http://localhost:8081/rest/books")
+        .then(response=>response.data)
+        .then((data)=>{this.setState({books:data});
+        });
+    }
     render() {
 
         return (
             <Container>
-                <Card.Header><FontAwesomeIcon icon={faList} /> Book List </Card.Header>
                 <Card >
+                    <Card.Header >&nbsp;&nbsp;
+                      <FontAwesomeIcon icon={faList} /> &nbsp;<b> Book List</b>
+
+                    </Card.Header>
+
                     <Card.Body>
                         <Table striped bordered hover size="sm">
-                            <thead class="table-primary">
+                            <thead className="table-primary">
                                 <tr>
 
                                     <th>Book Name</th>
@@ -24,11 +42,31 @@ class BookList extends React.Component {
                                 </tr>
                             </thead>
                             <tbody variant="secondary">
+                                {
+                                    this.state.books.length === 0 ?
+                                
                                 <tr align="center">
-                                    <td colSpan="6">No Books available.</td>
+                                        <td colSpan="6">No Books available.</td>                                  
+                                </tr> :
+                                this.state.books.map((book)=>
+                                <tr key={book.id}>
+                                    <td>
+                                        <Image src={book.coverPhotoURL} roundedCircle width="25" height="25"/>&nbsp;&nbsp;
+                                        {book.title}</td>
+                                    <td>{book.author}</td>
+                                    <td>{book.price}</td>
+                                    <td>{book.isbnNumber}</td>
+                                    <td>{book.tags}</td>
+                                    <td align="center">
+                                        <ButtonGroup >
+                                            <Button size="sm" className="btn-info"><FontAwesomeIcon icon={faEdit} /></Button>
+                                            <Button size="sm" className="btn-danger"><FontAwesomeIcon icon={faTrashAlt} /></Button>
+                                        </ButtonGroup>
+                                    </td>
                                 </tr>
+                                )
 
-
+                                }
                             </tbody>
                         </Table>
                     </Card.Body>
